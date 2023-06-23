fc4b3b672579f3c07e2f17f749b69645e95a8473
Jesse Cammarano <jcamma93@gmail.com>
2023-06-23 12:46:52 -0400

    main

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,17 @@ import PeopleCard from './PeopleCard';
 const App = () => {
     const [films, setFilms] = useState([]);
     const [people, setPeople] = useState([])
+    const [displayFilms, setDisplayFilms] = useState(false);
+    const [displayPeople, setDisplayPeople] = useState(false);
+    const setShowFilms = e => {
+        setDisplayFilms(true);
+        setDisplayPeople(false);
+    }
+
+    const setShowPeople = e => {
+        setDisplayPeople(true);
+        setDisplayFilms(false)
+    }
 
     useEffect(() => {
         fetch("http://api-ghibli.herokuapp.com/films")
@@ -12,16 +23,27 @@ const App = () => {
             .then(data => setFilms(data));
     }, []);
 
-    useEffect(()=> {
+
+    useEffect(() => {
         fetch('http://api-ghibli.herokuapp.com/people')
-            .then(res=> res.json())
+            .then(res => res.json())
             .then(data => setPeople(data));
     })
+
+    if (displayFilms == false && displayPeople == false) {
+        return (
+            <div>
+                <h1>Studio Ghibli Database</h1>
+                <button className="btn btn-primary" onClick={setShowFilms}>Display Films</button>
+                <button className="btn btn-primary" onClick={setShowPeople}>Display People</button>
+            </div>
+        )
+    }
 
     return (
         <div>
             <h1>Studio Ghibli Database</h1>
-            {films.map((flms) => (
+            {displayFilms && films.map((flms) => (
 
                 <FilmCard title={flms.title}
                     description={flms.description}
@@ -31,13 +53,13 @@ const App = () => {
 
             ))}
 
-            {people.map((ppl) => (
+            {displayPeople && people.map((ppl) => (
 
-                <PeopleCard name={ppl.name} 
-                            gender={ppl.gender} 
-                            age={ppl.age} 
-                            eye_color={ppl.eye_color} 
-                            hair_color={ppl.hair_color}/>
+                <PeopleCard name={ppl.name}
+                    gender={ppl.gender}
+                    age={ppl.age}
+                    eye_color={ppl.eye_color}
+                    hair_color={ppl.hair_color} />
 
             ))}
         </div>
